d2407198c51965426f7a6d45b72e132a1cc0d61e
Josse Filippi <76205885+Pepsci@users.noreply.github.com>
2021-12-11 19:02:36 +0100

    début création de la grille en js

---
+++ js/scripy.js
@@ -0,0 +1,32 @@
+const btn1 = document.getElementById('2').value;
+
+
+let x = btn1;
+let y = btn1;
+
+function generateMatrix(x, y){
+    let body = document.getElementsByTagName('body')[0];
+    let table = document.createElement('table');
+    let tbody = document.createElement('tbody');
+     x = btn1;
+     y = btn1;
+
+
+    for (let i = 0; i < x; i++) {
+        let row = document.createElement('tr');
+
+        for (let j = 0; j < y; j++) {
+            let cell = document.createElement('td');
+            let cellText = document.createTextNode(i*x + j+ 1);
+            cell.appendChild(cellText);
+            row.appendChild(cell);
+        }
+        tbody.appendChild(row)
+    }
+    table.appendChild(tbody);
+    body.appendChild(table);
+    table.setAttribute("border", 2);
+}
+
+
+document.getElementById('2').addEventListener("click", generateMatrix)
