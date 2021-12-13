fd2abe6a4c47e84ab4b537fec801be3efe2b0918
Pepsci <jossfilippi@gmail.com>
2021-12-13 16:10:01 +0100

    génération des 3 grille de difficulté reste a récupérer la valeur des 3 btn

--- js/scripy.js
+++ js/scripy.js
@@ -1,22 +1,50 @@
-const btn1 = document.getElementById('2').value;
+const btn = document.getElementById('16').value;
 
 
-let x = btn1;
+let x = btn;
 
-function generateMatrix(x){
+function generatePuzzle(x){
     let puzzle = document.querySelector('.puzzle-container');
-    let puzzleChild = puzzle.childNodes;
-    x = btn1;
+         puzzle.innerHTML = "";
+    let block0 = document.createElement('div');
+        block0.className = 'block-0 block'
+        let puzzleChild = puzzle.childNodes;
+        puzzle.appendChild(block0);
+        divSize(block0)
+        
+    x = btn;
+
+    let random = [];
+while(random.length < x){
+    let r = Math.floor(Math.random() * x) + 1;
+    if(random.indexOf(r) === -1) random.push(r);
+}
+console.log(random);
     
     for (let i = 0; i < x; i++) {
         let block = document.createElement('div');
-        let blockText = document.createTextNode(i + 0);
-        block.className = 'block'
-        block.appendChild(blockText)
-        puzzle.appendChild(block)
-        puzzleChild.className = 'block-0'    
+        let blockText = document.createTextNode(random[i]);
+        block.className = `block-${i + 1} block`;
+        block.appendChild(blockText);
+        puzzle.appendChild(block);
+        divSize(block)
+ 
+    }
+}
+
+function divSize(div){
+    if(x <= 3){
+        div.setAttribute("style","width:var(--size-puzzle-4); height:var(--size-puzzle-4)" );
+    }else if (x > 3 && x <= 9) {
+        div.setAttribute("style","height:var(--size-puzzle-9); width:var(--size-puzzle-9)" );
+    }else if (x > 9){
+        div.setAttribute("style"," width:var(--size-puzzle-16); height:var(--size-puzzle-16)" );
     }
 }
 
 
-document.getElementById('2').addEventListener("click", generateMatrix)
+document.getElementById('2').addEventListener("click", generatePuzzle)
+document.getElementById('9').addEventListener("click", generatePuzzle)
+document.getElementById('16').addEventListener("click", generatePuzzle)
+
+// document.querySelectorAll('btn-choice').addEventListener('click', generatePuzzle)
